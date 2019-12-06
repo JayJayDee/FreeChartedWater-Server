d8983ac453b3759fa6896d89d5c8e598b048184c
JayJayDee <jindongp@gmail.com>
2019-12-06 12:46:45 +0900

    defined more entity to GraphQL schema.

--- src/libs/entities/product/product.ts
+++ src/libs/entities/product/product.ts
@@ -1,19 +1,28 @@
 import { Entity, PrimaryGeneratedColumn, ManyToOne } from 'typeorm';
 import { Ship } from '../ship';
 import { City } from '../city';
+import { ObjectType, Field, ID } from 'type-graphql';
 
 @Entity()
+@ObjectType()
 export class Product {
 
   @PrimaryGeneratedColumn()
+  @Field((type) => ID)
   public no: number;
 
   @ManyToOne((type) => Ship, {
     nullable: true,
   })
+  @Field((type) => Ship, {
+    nullable: true,
+  })
   public loadedBy: Ship | null;
 
   @ManyToOne((type) => City, {
+    nullable: true,
+  })
+  @Field((type) => City, {
     nullable: true,
   })
   public producedBy: City | null;
--- src/libs/entities/ship/ship.ts
+++ src/libs/entities/ship/ship.ts
@@ -3,22 +3,29 @@ import { User } from '../user';
 import { Product } from '../product';
 import { Fleet } from '../fleet';
 import { BaseShip } from './base-ship';
+import { ObjectType, Field, ID } from 'type-graphql';
 
 @Entity()
+@ObjectType()
 export class Ship {
 
   @PrimaryGeneratedColumn()
+  @Field((type) => ID)
   public no: number;
 
   @ManyToOne((type) => BaseShip)
+  @Field((type) => BaseShip)
   public base: BaseShip;
 
   @ManyToOne((type) => User)
+  @Field((type) => User)
   public owner: User;
 
   @ManyToOne((type) => Fleet)
+  // TODO: graphQL Field to be connected
   public fleet: Fleet;
 
   @OneToMany((type) => Product, (product) => product.loadedBy)
+  @Field((type) => [ Product ])
   public products: Product[];
 }
