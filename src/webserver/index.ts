8b9330f22f49ea0bc92aea68cd79b574f47945a4
JayJayDee <jindongp@gmail.com>
2019-12-06 23:29:46 +0900

    added log on lib::typeorm-initiator added typeorm-initiator on webserver.

--- src/webserver/index.ts
+++ src/webserver/index.ts
@@ -2,14 +2,18 @@ import { ApolloServer } from 'apollo-server';
 import { graphQLSchema } from './graphql-schema';
 import { log } from '../libs/logger';
 import { GeneratingSchemaError } from 'type-graphql';
+import { initTypeORM } from '../libs/typeorm-initiator';
+import { AllEntities as entities } from '../libs/entities';
 
 const tag = '[graphql-webserver]';
 
 (async () => {
 
   try {
-    const schema = await graphQLSchema();
+    const logging = true;
+    await initTypeORM({ logging, entities });
 
+    const schema = await graphQLSchema();
     const server = new ApolloServer({
       schema,
       playground: true,
