d4f08ff94246d719fe45eabfe2518468c53699e2
JayJayDee <jindongp@gmail.com>
2019-12-08 11:24:40 +0900

    added base-*-resolvers, added @FieldResolvers()

--- src/webserver/resolvers/champion-resolver.ts
+++ src/webserver/resolvers/champion-resolver.ts
@@ -2,6 +2,20 @@ import { Resolver, Root, FieldResolver } from 'type-graphql';
 import { Champion, BaseChampion } from '../../libs/entities';
 import { getRepository } from 'typeorm';
 
+@Resolver((of) => BaseChampion)
+export class BaseChampionResolver {
+
+  @FieldResolver((type) => [ Champion ])
+  public async champions(@Root() base: BaseChampion) {
+    const champions = await getRepository(Champion).find({
+      where: {
+        base,
+      },
+    });
+    return champions;
+  }
+}
+
 @Resolver((of) => Champion)
 export class ChampionResolver {
 
