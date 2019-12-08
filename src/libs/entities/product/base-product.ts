623ed8ea3e55f0900b2dc6a825f433d4eb027a50
JayJayDee <jindongp@gmail.com>
2019-12-08 10:47:39 +0900

    added product-status added champion/product level

--- src/libs/entities/product/base-product.ts
+++ src/libs/entities/product/base-product.ts
@@ -1,6 +1,34 @@
 import { Entity, PrimaryGeneratedColumn, Column, OneToMany } from 'typeorm';
-import { ObjectType, Field, ID } from 'type-graphql';
+import { ObjectType, Field, ID, registerEnumType } from 'type-graphql';
 import { Product } from './product';
+
+enum ProductTypeEnum {
+  LUXURY = 'LUXURY',
+  CLOTH = 'CLOTH',
+  GROCERY = 'GROCERY',
+  LIQUOR = 'LIQUOR',
+  SPICE = 'SPICE',
+  MINERAL = 'MINERAL',
+  CROP = 'CROP',
+  FANCY_GOODS = 'FANCY_GOODS',
+  DYE = 'DYE',
+}
+
+registerEnumType(ProductTypeEnum, {
+  name: 'ProductType',
+  description: 'product type',
+});
+
+type ProductType =
+  'LUXURY' |
+  'CLOTH' |
+  'GROCERY' |
+  'LIQUOR' |
+  'SPICE' |
+  'MINERAL' |
+  'CROP' |
+  'FANCY_GOODS' |
+  'DYE';
 
 @Entity()
 @ObjectType()
@@ -13,6 +41,13 @@ export class BaseProduct {
   @Column()
   @Field()
   public nameCode: string;
+
+  @Column({
+    type: 'enum',
+    enum: ProductTypeEnum,
+  })
+  @Field((type) => ProductTypeEnum)
+  public type: ProductType;
 
   @Column()
   @Field()
--- src/libs/entities/product/product.ts
+++ src/libs/entities/product/product.ts
@@ -1,8 +1,20 @@
-import { Entity, PrimaryGeneratedColumn, ManyToOne, Column } from 'typeorm';
+import { Entity, PrimaryGeneratedColumn, ManyToOne, Column, AfterLoad } from 'typeorm';
 import { Ship } from '../ship';
 import { City } from '../city';
-import { ObjectType, Field, ID } from 'type-graphql';
+import { ObjectType, Field, ID, registerEnumType } from 'type-graphql';
 import { BaseProduct } from './base-product';
+
+enum ProductStatusEnum {
+  SPAWNED = 'SPAWNED',
+  SHIPPED = 'SHIPPED',
+}
+
+type ProductStatus = 'SPAWNED' | 'SHIPPED';
+
+registerEnumType(ProductStatusEnum, {
+  name: 'ProductStatus',
+  description: 'product status, SPAWNED | SHIPPED',
+});
 
 @Entity()
 @ObjectType()
@@ -15,6 +27,10 @@ export class Product {
   @Column()
   @Field()
   public price: number;
+
+  @Column()
+  @Field()
+  public level: number;
 
   @ManyToOne((type) => BaseProduct)
   @Field((type) => BaseProduct)
@@ -35,4 +51,16 @@ export class Product {
     nullable: true,
   })
   public producedBy: City | null;
+
+  @Field((type) => ProductStatusEnum)
+  public status: ProductStatus;
+
+  @AfterLoad()
+  private updateStatus() {
+    if (this.producedBy) {
+      this.status = 'SPAWNED';
+    } else {
+      this.status = 'SHIPPED';
+    }
+  }
 }
