3f90bd50b36acf6c31774f4ff7eaceec1172310b
JayJayDee <jindongp@gmail.com>
2019-12-10 00:01:08 +0900

    added city-resolver, country-resolver.

--- src/webserver/resolvers/index.ts
+++ src/webserver/resolvers/index.ts
@@ -1,9 +1,11 @@
 import { UserResolver } from './user-resolver';
 import { ChampionResolver } from './champion-resolver';
 import { BaseShipResolver, ShipResolver } from './ship-resolver';
-import { FleetResolver } from './fleet';
 import { BaseItemResolver, ItemResolver } from './item-resolver';
 import { BaseProductResolver, ProductResolver } from './product-resolver';
+
+import { FleetResolver } from './fleet';
+import { CountryResolver, CityResolver } from './city';
 
 export const graphQLResolvers =
   () => {
@@ -14,5 +16,6 @@ export const graphQLResolvers =
       FleetResolver,
       BaseShipResolver, ShipResolver,
       BaseProductResolver, ProductResolver,
+      CountryResolver, CityResolver,
     ];
   };
